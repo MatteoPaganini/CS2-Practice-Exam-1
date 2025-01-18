4306281b1d714bbec453e9cdc673ab4be03cbf3e
MatteoPaganini <matteo_paganini25@milton.edu>
2025-01-18 11:34:33 -0500

    worked on review

---
+++ src/Alien.java
@@ -0,0 +1,43 @@
+public class Alien {
+
+    private int energyLevel;
+    private int hostilityIndex;
+
+//    public static void main(String[] args) {
+//        System.out.println("");
+//    }
+
+    public Alien(int pEnergyLevel, int pHostilityIndex){
+        energyLevel = pEnergyLevel;
+        hostilityIndex = pHostilityIndex;
+
+        printInfo();
+    }
+
+    public int getEnergyLevel(){
+        return energyLevel;
+    }
+
+    public void setEnergyLevel(int x){
+        energyLevel = x;
+    }
+
+    public int getHostilityIndex(){
+        return hostilityIndex;
+    }
+
+    public void setHostilityIndex(int y){
+        this.hostilityIndex = y;
+    }
+
+    public void printInfo(){
+        System.out.println("The energy level is: " + getEnergyLevel());
+        System.out.println("the hostility index is: " + getHostilityIndex());
+        //another to do this
+        //System.out.println("The hostility index is " + hostilityIndex);
+        System.out.println("Energy level is set to: " + energyLevel);
+        System.out.println("Hostility index is set to: " + hostilityIndex);
+
+    }
+
+}
--- src/StarshipExplorer.java
+++ src/StarshipExplorer.java
@@ -1,5 +1,41 @@
+import java.sql.Array;
+
 public class StarshipExplorer {
+
+    public static void main(String[] args) {
+        StarshipExplorer starshipExplorer = new StarshipExplorer();
+    }
+
+    public StarshipExplorer(){
+        System.out.println("Hello World! Good luck on your exams!");
+
+        System.out.println(decipher(alienLanguage));
+
+        Alien[] aliens = new Alien[50];
+        for (int i = 0; i < 50; i++){
+            aliens[i] = new Alien();
+        }
+    }
+
 
     private String alienLanguage = "Vrlnq*T xm!ow b*aF#nmpq*k^s jfy*e* *m&xlqw*e tT@br* lm*t oT%*ovnzr* oY#rwqs*y uo@L*oe^t aE!wqlm*u*r aD@rzxs* eB&vnyp Rlkzx*l W tnl@qp*e eXm*a#kyr*d aM^yplw o*e&mnbs*r eI!k*,srn* pC#txry*B cO@pb*elm Hlmpq*y P tn!*ojlwl rE#kxtm*n eF^l*cqrx tO&wtnq*e fC*!xtpm oK#xrnl nT@klpm cT@vrqp";
 
+
+    public String decipher (String languageSample){
+        String decodedString = "";
+
+        while(languageSample.contains("*")){
+            int star = languageSample.indexOf("*");
+            String letter = languageSample.substring(star +1, star +2);
+            System.out.println(letter);
+
+            decodedString += letter;
+            languageSample = languageSample.substring(star+2);
+        }
+
+        return decodedString;
+
+    }
+
 }
+
